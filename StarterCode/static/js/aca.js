d105f5e1b6c4bd81ae4080da999314b8e883195e
Shanil Lobanwala <slobanwala@ad.thinkmcs.com>
2021-01-16 23:18:43 -0600

    Added references txt

--- StarterCode/static/js/aca.js
+++ StarterCode/static/js/aca.js
@@ -2,3 +2,13 @@
 // Date: Jan 15, 2020
 // Project Name: Plot.ly Homework - Belly Button Biodiversity
 // File: Advanced Challenge Assignment/aca.js
+
+
+
+// Function to set up gauge with data
+function init() {
+  // build gauge
+}
+
+// initalize function
+init();
--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,12 @@
 // Date: Jan 15, 2020
 // Project Name: Plot.ly Homework - Belly Button Biodiversity
 // File: Main logic/app.js
+
+
+
+// Create an initalize function
+function init() {
+
+}
+// Call said initialize function
+init();
