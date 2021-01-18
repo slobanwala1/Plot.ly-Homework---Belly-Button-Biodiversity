eac3b93f2d4d9033e41992544e2ecda1c08e529a
Shanil Lobanwala <slobanwala@ad.thinkmcs.com>
2021-01-18 03:55:59 -0600

    Fixed initial func bugs

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,23 +3,29 @@
 // Project Name: Plot.ly Homework - Belly Button Biodiversity
 // File: Main logic/app.js
 
+// function to generate the bargraph
+function buildBarGraph(bbId) {
 
+}
 
 // Create an initalize function
 function init() {
+
   // Initial position of dropdown(first element)
   var dropDown = d3.select("#selDataset");
-
-  d3.json("samples.json").then((data) => {
-    var names = data.names;
-    names.forEach((name) => {
-      // console.log(sample);
-      // populate dropdown with all the names/numbers
-      dropDown.append("option").text(name).property("value", name);
+  d3.json("samples.json").then((bbSamples) => {
+    var bbId = bbSamples.names;
+    bbId.forEach((id) => {
+      // console.log(name);
+      // populate dropdown with all the ids
+      dropDown.append("option").text(id).property("value", id);
     })
+
+    // Populate the bar chart with the first name/number as the starting point on load.
+    var firstId = bbId[0];
+    // console.log(firstId);
+    buildBarGraph(firstId);
   });
-
-
 }
 
 // Function that index.html element selDataset references
